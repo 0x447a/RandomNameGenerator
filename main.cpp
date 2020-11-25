343e7378f2cedc3dc06158580a450b4fd4500418
Joshua Felix <64223641+0x447a@users.noreply.github.com>
2020-11-25 11:05:59 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//#include <random>
+//#include <time.h>
+
+/*this right here is supposed to be the first and last names that it's supposed to take and organize
+into actual first and last names
+*/
+
+string names (int firstname_length, int lastname_length){
+  char first_name [10] = {"Jazlene","Fernanda","Jaylynn",
+                        "Ryann","Noah","Camden","Rhianna",
+                         "Gabrielle","Jenny","Emanuel"};
+
+  char last_name [10] = {"Ortiz","Morton","Moody",
+                         "Rocha","Murray","Stewart",
+                        "Franco","Lutz","Barber","Wang"};
+}
+
+
+int main(int argc, char *argv){
+for (j = 0, j < 10, j++){
+  //cout << person.first_name [10] && person.last_name [10] << "\n";
+}
+return 0;
+}
